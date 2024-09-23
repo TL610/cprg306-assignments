bebe8cddbf2b94d239a0798831cc3768d7066acd
Teresa Lok <a610tl@gmail.com>
2024-09-23 15:21:12 -0600

    title styling

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main>
-      <h1 className="">Shopping List</h1>
+      <h1 className="text-4xl font-bold m-2 mb-4">Shopping List</h1>
       <ItemList />
     </main>
   );
